83a45a3bea5e7bf6edf375a1297fdcc93059c570
ghizlane rahmouni <ghlaneiz022@gmail.com>
2025-02-28 22:58:27 +0000

    oauth password request form

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,14 +2,15 @@ from fastapi import Depends, APIRouter, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from .. import schemas, models, utils, oauth2
 from ..database import get_db
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(
     tags=["Authentication"],
 )
 
 router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.Users).filter(models.Users.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.Users).filter(models.Users.email == user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     if not utils.verify_password(user_credentials.password, user.password):
