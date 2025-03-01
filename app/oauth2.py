b19f0ea7a01160d2a7c2419db275763eef58bd17
ghizlane rahmouni <ghlaneiz022@gmail.com>
2025-03-01 12:40:34 +0000

    fetching user in protected route

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,9 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -39,6 +40,10 @@ def verify_jwt_token(token: str , credentials_exception):
     except JWTError:
         raise credentials_exception
     
-def get_current_user(token : str = Depends(oauth2_scheme)):
+def get_current_user(token : str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credientials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
-    return verify_jwt_token(token, credientials_exception)
+    token = verify_jwt_token(token, credientials_exception)
+    user = db.query(database.Users).filter(database.Users.id == token.id).first()
+    if user is None:
+        raise credientials_exception
+    return user
