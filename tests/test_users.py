e8614149a3d830cfa927ca47f877dd0dace444c2
ghizlane rahmouni <ghlaneiz022@gmail.com>
2025-03-06 11:09:33 +0000

    conftest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,22 +1,8 @@
 from app import schemas
-from .database import session , client
 import pytest
 from jose import jwt
 from app.config import settings
 
-
-@pytest.fixture(scope="function")
-def test_user(client):
-    user_data = {
-        "username": "m",
-        "email": "m@email.com",
-        "password": "password123",
-    }
-    res = client.post("/users", json=user_data)
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user["password"] = user_data["password"]
-    return new_user
 
 
 def test_create_user(client):
